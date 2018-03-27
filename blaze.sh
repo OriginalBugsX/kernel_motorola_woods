c62ab90d80f34473a85cff0834853fbf8f4a08f5
Izaq Kull <izaqkull@yahoo.com>
2018-03-27 01:17:27 +0000

    update for Oreo

--- blaze.sh
+++ blaze.sh
@@ -19,7 +19,7 @@ mkdir -p outdir
 #toolchain , custom build_user , custom build_host , arch
 export ARCH=arm
 export ARCH_MTK_PLATFORM=mt6735
-export CROSS_COMPILE=/home/zac6ix/nana/woods/arm-eabi-4.9/bin/arm-eabi-
+export CROSS_COMPILE=/home/izaqkull/Documents/wood/working_dir/prebuilts_gcc_arm_eabi_4.9/bin/arm-eabi-
 export KBUILD_BUILD_USER="izaqKull"
 export KBUILD_BUILD_HOST="devStudGH"
 
@@ -47,10 +47,10 @@ fi
 zip_zak ()
 {
 echo -e "$cyan***********************************************"
-echo "          ZIpping Blaze™ Kernel...          "
+echo "          Zipping Blaze™ Kernel...          "
 echo -e "***********************************************$nocol"
 echo ""
-echo -e "$yellow Putting iykonzBlaze™ Kernel in Recovery Flashable Zip $nocol"
+echo -e "$yellow Putting Blaze™ Kernel in Recovery Flashable Zip $nocol"
 #using lazy kernel flasher
 cd outdir
 cd iykonzBlaze
@@ -61,7 +61,7 @@ cd iykonzBlaze
     else
     make
     mkdir -p out_done
-    cp Jennie.N7x_woods*zip* out_done
+    cp Jennie.O8x_woods*zip* out_done
     cd ../../
     sleep 0.6;
     echo ""
@@ -76,7 +76,7 @@ cd iykonzBlaze
     echo "          Uploading Blaze™ Kernel as zip...          "
     echo -e "***********************************************$nocol"
     echo ""
-    curl --upload-file outdir/iykonzBlaze/out_done/Jennie.N7x_woods*.zip https://transfer.sh/Jennie.N7x_woods_Nougat_$BUILD_START.zip
+    curl --upload-file outdir/iykonzBlaze/out_done/Jennie.O8x_woods*.zip https://transfer.sh/Jennie.O8x_woods_Oreo_$BUILD_START.zip
     echo ""
     echo ""
     echo " Uploading Done !!!"
